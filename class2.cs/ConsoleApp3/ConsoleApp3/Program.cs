d702c66f2d3836256d4b08b08aba4286efe13c0c
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-11 11:21:12 +0400

    aztu.code

---
+++ class2.cs/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prd=new Product();
+            prd.name = "kartof";
+            prd.BrandName = "bakY";
+            prd.price = 0.80;
+            Product prd1=new Product();
+            prd.name = "sogan";
+            prd.BrandName = "ismayilli";
+            prd.price = 0.5;
+        }
+        static string[] arry(double min = 0, double max = 10)
+        {
+            Product[] array = new Product[2];
+            for (int i = 0; i < array.Length; i++)
+            {
+               if(array[i] < min)
+            }
+        }
+    }
+}

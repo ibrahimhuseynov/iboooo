a5782fc84e236685513dce7e0098543a6719b469
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-29 11:11:49 +0400

    jsfhwf dhfaj

---
+++ task28.cs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("FullName daxil edin :");
+                Student.FullName=Console.ReadLine();
+                
+            } while (!Student.CheckFullname(Student.FullName));
+            do
+            {
+                Console.WriteLine("GroupNo daxil edin :");
+               Student.GroupNo=Console.ReadLine();
+
+            }while(!Student.CheckGroupNo(Student.GroupNo));  
+            Console.WriteLine("Age daxil edin :");
+            Student.Age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("FullName :" + Student.FullName);
+            Console.WriteLine("GruoNo :" + Student.GroupNo);
+            Console.WriteLine("Age :" + Student.Age);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ task28.cs/ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal static  class Student
+    {
+        public static  string FullName { get; set; }    
+        public static int Age { get; set; }
+        public  static string GroupNo { get; set; }
+        public static bool CheckGroupNo( string Grupnomber)
+        {
+            if (char.IsUpper(Grupnomber[0]))
+            {
+                if(char.IsDigit(Grupnomber[1]))
+                {
+                    if (char.IsDigit(Grupnomber[2]))
+                    {
+                        if (char.IsDigit(Grupnomber[3]))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;   
+        }
+        public static bool  CheckFullname(this string str)
+        {
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[0]))
+                {
+                    if(str[i]==' ')
+                    {
+                        if (char.IsUpper(str[i + 1]))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+     
+}
